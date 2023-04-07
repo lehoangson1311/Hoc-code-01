228c24cf1128636a03fd2bc6e7a1286c7ede0f48
lehoangson1311 <lehoangson1311@gmail.com>
2023-04-07 18:28:19 +0700

    thuc-thanh-tinh-diem-js

---
+++ sesion05/script.js
@@ -0,0 +1,26 @@
+// alert("hello external");
+var subject = "javascript";
+// hien thi o info cua console
+console.log(subject);
+
+// hien thi o warning cua console
+console.warn("cảnh báo nhé !!!");
+
+// hien thi o error cua con sole
+console.error("lỗi rồi các bạn ơi");
+
+// khai bao
+var age;
+
+//gán
+age = 18;
+
+console.log("giá trị của age");
+
+// kiem tra kieu du lieu
+
+console.log("Kiểm tra kiểu dữ liệu", typeof age);
+{
+  let age = 18;
+  age = 30;
+}
